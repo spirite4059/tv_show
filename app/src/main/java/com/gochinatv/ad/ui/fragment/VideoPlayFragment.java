30afd3bb5131a31725b133022e324c0fa26ea00f
fuqiang <fuqiang@gochinatv.com>
2016-03-16 15:42:12 +0800

    tijiao

--- app/src/main/java/com/gochinatv/ad/ui/fragment/VideoPlayFragment.java
+++ app/src/main/java/com/gochinatv/ad/ui/fragment/VideoPlayFragment.java
@@ -1,7 +1,34 @@
 package com.gochinatv.ad.ui.fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gochinatv.ad.base.BaseFragment;
+
 /**
  * Created by zfy on 2016/3/16.
  */
-public class VideoPlayFragment {
+public class VideoPlayFragment extends BaseFragment{
+
+
+    @Override
+    protected View initLayout(LayoutInflater inflater, ViewGroup container) {
+        return null;
+    }
+
+    @Override
+    protected void initView(View rootView) {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void bindEvent() {
+
+    }
 }
