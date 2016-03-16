fa0745946d8d4853e787c3d32c4f252630b6f9ab
zhufengyuan <zhu_feng_yuan@163.com>
2016-03-16 15:30:12 +0800

    提交FRAGMENT

---
+++ app/src/main/java/com/gochinatv/ad/ui/fragment/VideoPlayFragment.java
@@ -0,0 +1,7 @@
+package com.gochinatv.ad.ui.fragment;
+
+/**
+ * Created by zfy on 2016/3/16.
+ */
+public class VideoPlayFragment {
+}
--- app/src/main/java/com/gochinatv/ad/ui/fragment/阿斯蒂芬啊.java
+++
@@ -1,7 +0,0 @@
-package com.gochinatv.ad.ui.fragment;
-
-/**
- * Created by fq_mbp on 16/3/16.
- */
-public class 阿斯蒂芬啊 {
-}
