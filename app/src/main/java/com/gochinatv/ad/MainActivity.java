e76239cbb419aae2338052336c572e6300432d42
zhufengyuan <zhu_feng_yuan@163.com>
2016-04-08 10:24:24 +0800

    root放到网络请求前面

--- app/src/main/java/com/gochinatv/ad/MainActivity.java
+++ app/src/main/java/com/gochinatv/ad/MainActivity.java
@@ -19,14 +19,22 @@ import com.gochinatv.ad.tools.DownloadUtils;
 import com.gochinatv.ad.tools.LogCat;
 import com.gochinatv.ad.tools.RootUtils;
 import com.gochinatv.ad.tools.SharedPreference;
+import com.gochinatv.ad.ui.fragment.ADFourFragment;
+import com.gochinatv.ad.ui.fragment.ADThreeFragment;
+import com.gochinatv.ad.ui.fragment.ADTwoFragment;
 import com.gochinatv.ad.ui.fragment.AdOneFragment;
 import com.httputils.http.response.UpdateResponse;
 import com.okhtttp.OkHttpCallBack;
 import com.okhtttp.OkHttpUtils;
+import com.okhtttp.response.ADDeviceDataResponse;
+import com.okhtttp.response.LayoutResponse;
+import com.okhtttp.response.ScreenShotResponse;
+import com.okhtttp.service.ADHttpService;
 import com.tools.HttpUrls;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -45,14 +53,31 @@ public class MainActivity extends Activity {
     private UpdateResponse.UpdateInfoResponse updateInfo;
     private AdOneFragment adOneFragment;
 
+    /**
+     * 是否升级接口成功
+     */
+    private boolean isUpgradeSucceed;
+    /**
+     * 是否有新版本升级
+     */
+    private boolean isHasUpgrade;
+
+
+    /**
+     * 是否请求广告体接口成功
+     */
+    private boolean isGetDerviceSucceed;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         view = findViewById(R.id.root_main);
         loadingView = (LinearLayout) findViewById(R.id.loading);
-        doHttpUpdate(this);
         RootUtils.hasRootPerssion();//一开始就请求root
+        doHttpUpdate(this);
 
         //        //新包下载完成得安装
 //        if(RootUtils.hasRootPerssion()){
@@ -75,13 +100,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
-
+        LogCat.e("当弹出弹出root框时，MainActivity是否 onStop");
     }
 
     /**
      * 检查是否有版本更新
      */
-
     private int reTryTimes;
     protected void doHttpUpdate(final Context context) {
         Map<String, String> map = new HashMap<>();
@@ -146,11 +170,14 @@ public class MainActivity extends Activity {
                             double netVersonCode = Integer.parseInt(updateInfo.versionCode);
                             // 检测是否要升级
                             try {
+                                //升级接口成功
+                                isUpgradeSucceed = true;
                                 if (DataUtils.getAppVersion(context) < netVersonCode) { // 升级
                                     // 升级
                                     // 下载最新安装包，下载完成后，提示安装
                                     LogCat.e("需要升级。。。。。");
                                     // 去下载当前的apk
+                                    isHasUpgrade = true;
                                     downloadAPK();
                                     // 加载布局.但是不让AdOneFragment，下载视频
                                     loadFragment(true);
@@ -158,7 +185,6 @@ public class MainActivity extends Activity {
                                 } else {
                                     // 不升级,加载布局
                                     LogCat.e("无需升级。。。。。");
-
                                     // 5.清空所有升级包，为了节省空间
                                     DeleteFileUtils.getInstance().deleteFile(DataUtils.getSdCardFileDirectory() + Constants.FILE_DIRECTORY_APK);
                                     LogCat.e("清空升级apk.....");
@@ -184,6 +210,8 @@ public class MainActivity extends Activity {
                             if (reTryTimes > 4) {
                                 reTryTimes = 0;
                                 LogCat.e("升级接口已连续请求3次，不在请求");
+                                //升级接口成功
+                                isUpgradeSucceed = true;
                                 loadFragment(false);
                             } else {
                                 LogCat.e("进行第 " + reTryTimes + " 次重试请求。。。。。。。");
@@ -222,6 +250,91 @@ public class MainActivity extends Activity {
     }
 
 
+    /**
+     * 加载fragment
+     * @param isDownload
+     */
+    private LayoutResponse oneLayout;//广告一布局
+    private LayoutResponse twoLayout;//广告二布局
+    private LayoutResponse threeLayout;//广告三布局
+    private LayoutResponse fourLayout;//广告四布局
+    private void loadFragmentTwo(boolean isDownload) {
+        //当升级和广告体接口都完成后，才加载布局
+        if (isUpgradeSucceed && isGetDerviceSucceed) {
+            loadingView.setVisibility(View.GONE);
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+
+            adOneFragment = new AdOneFragment();
+            if (isDownload) {
+                adOneFragment.setIsDownloadAPK(true);
+            }
+            if (screenShot != null) {
+                //截屏的参数
+                //adOneFragment.
+            }
+            //设置布局参数
+            if (LayoutResponses != null && LayoutResponses.size() > 0) {
+                int size = LayoutResponses.size();
+                for (int i = 0; i < size; i++) {
+                    if ("1".equals(LayoutResponses.get(i).adType)) {
+                        oneLayout = LayoutResponses.get(i);
+                    } else if ("2".equals(LayoutResponses.get(i).adType)) {
+                        twoLayout = LayoutResponses.get(i);
+                    } else if ("3".equals(LayoutResponses.get(i).adType)) {
+                        threeLayout = LayoutResponses.get(i);
+                    } else if ("4".equals(LayoutResponses.get(i).adType)) {
+                        fourLayout = LayoutResponses.get(i);
+                    }
+                }
+            }
+
+            //广告一
+            if (oneLayout != null) {
+                if (!TextUtils.isEmpty(oneLayout.adWidth) && !TextUtils.isEmpty(oneLayout.adHeight)
+                        && !TextUtils.isEmpty(oneLayout.adTop) && !TextUtils.isEmpty(oneLayout.adLeft)) {
+                    //此时加载广告一
+                    //adOneFragment.setLayoutResponse(oneLayout);
+                    ft.add(R.id.root_main, adOneFragment);
+                }
+            }
+
+            //广告二
+            if (twoLayout != null) {
+                if (!TextUtils.isEmpty(twoLayout.adWidth) && !TextUtils.isEmpty(twoLayout.adHeight)
+                        && !TextUtils.isEmpty(twoLayout.adTop) && !TextUtils.isEmpty(twoLayout.adLeft)) {
+                    //此时加载广告二
+                    ADTwoFragment adTwoFragment = new ADTwoFragment();
+                    // adTwoFragment.setLayoutResponse(twoLayout);
+                    ft.add(R.id.root_main, adTwoFragment);
+                }
+            }
+
+            //广告三
+            if (threeLayout != null) {
+                if (!TextUtils.isEmpty(threeLayout.adWidth) && !TextUtils.isEmpty(threeLayout.adHeight)
+                        && !TextUtils.isEmpty(threeLayout.adTop) && !TextUtils.isEmpty(threeLayout.adLeft)) {
+                    //此时加载广告三
+                    ADThreeFragment adThreeFragment = new ADThreeFragment();
+                    // adThreeFragment.setLayoutResponse(threeLayout);
+                    ft.add(R.id.root_main, adThreeFragment);
+                }
+            }
+
+            //广告四
+            if (fourLayout != null) {
+                if (!TextUtils.isEmpty(fourLayout.adWidth) && !TextUtils.isEmpty(fourLayout.adHeight)
+                        && !TextUtils.isEmpty(fourLayout.adTop) && !TextUtils.isEmpty(fourLayout.adLeft)) {
+                    //此时加载广告四
+                    ADFourFragment adFourFragment = new ADFourFragment();
+                    adFourFragment.setLayoutResponse(fourLayout);
+                    ft.add(R.id.root_main, adFourFragment);
+                }
+            }
+
+            ft.commit();
+        }
+    }
 
 
     /**
@@ -247,14 +360,112 @@ public class MainActivity extends Activity {
             public void onDownloadFileError(int errorCode, String errorMsg) {
                 //通知AdOneFragment去下载视频
                 LogCat.e("下载apk出现错误");
-                if(adOneFragment != null){
+                if (adOneFragment != null) {
                     adOneFragment.startDownloadVideo();
                 }
 
             }
         });
 
+    }
+
+
+    /**
+     * 请求广告体接口---布局大小
+     */
+
+    //布局数据
+    private  ArrayList<LayoutResponse> LayoutResponses;
+    //截屏数据
+    private ScreenShotResponse screenShot;
+    private int reTryTimesTwo;
+    private void doGetDerviceInfo(){
+        ADHttpService.doHttpGetDeviceInfo(this, new OkHttpCallBack<ADDeviceDataResponse>() {
+            @Override
+            public void onSuccess(String url, ADDeviceDataResponse response) {
+                LogCat.e("doGetDerviceInfo url:  " + url);
+                if (isFinishing()) {
+                    return;
+                }
+
+                if (response == null || !(response instanceof ADDeviceDataResponse)) {
+                    LogCat.e("请求广告体接口失败");
+                    doError();
+                    return;
+                }
+
+                if ("1".equals(response.status) == false) {
+                    LogCat.e("请求广告体接口失败 status = 1");
+                    doError();
+                    return;
+                }
+
+                if(response.screenShot != null){
+                    screenShot = response.screenShot;
+                }
+
+                if(response.layout != null){
+                    LayoutResponses = response.layout;
+                }
+
+                //广告体接口成功
+                isGetDerviceSucceed = true;
+                //加载布局
+                loadFragmentTwo(isHasUpgrade);
+
+            }
+
+
+
+            private void doError() {
+                if (!isFinishing()) {
+                    // 做不升级处理, 继续请求广告视频列表
+                    reTryTimesTwo++;
+                    if (reTryTimesTwo > 4) {
+                        reTryTimesTwo = 0;
+                        LogCat.e("升级接口已连续请求3次，不在请求");
+                        //广告体接口成功
+                        isGetDerviceSucceed = true;
+                        //加载布局
+                        loadFragmentTwo(isHasUpgrade);
+                    } else {
+                        LogCat.e("进行第 " + reTryTimesTwo + " 次重试请求。。。。。。。");
+                        doGetDerviceInfo();
+                    }
+                }
+            }
+
+
+            @Override
+            public void onError(String url, String errorMsg) {
+
+                LogCat.e("请求接口出错，无法升级。。。。。" + url);
+                doError();
+
+
+            }
+        });
+
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- app/src/main/java/com/gochinatv/ad/ui/fragment/ADFourFragment.java
+++ app/src/main/java/com/gochinatv/ad/ui/fragment/ADFourFragment.java
@@ -2,14 +2,21 @@ package com.gochinatv.ad.ui.fragment;
 
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.gochinatv.ad.R;
 import com.gochinatv.ad.base.BaseFragment;
+import com.gochinatv.ad.tools.DataUtils;
 import com.gochinatv.ad.tools.LogCat;
 import com.gochinatv.ad.ui.view.AutoTextView;
+import com.okhtttp.OkHttpCallBack;
+import com.okhtttp.response.ADFourResponse;
+import com.okhtttp.response.LayoutResponse;
+import com.okhtttp.service.ADHttpService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +34,48 @@ public class ADFourFragment extends BaseFragment {
     private boolean isCycleText;
     private Timer cycleTextTimer;
 
+    public LayoutResponse getLayoutResponse() {
+        return layoutResponse;
+    }
+
+    public void setLayoutResponse(LayoutResponse layoutResponse) {
+        this.layoutResponse = layoutResponse;
+    }
+
+    private LayoutResponse layoutResponse;
+
     @Override
     protected View initLayout(LayoutInflater inflater, ViewGroup container) {
-        return inflater.inflate(R.layout.fragment_ad_four,container,false);
+
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.fragment_ad_four, container, false);
+        if(layoutResponse != null){
+
+            if(!TextUtils.isEmpty(layoutResponse.adWidth) && !TextUtils.isEmpty(layoutResponse.adHeight)
+                    && !TextUtils.isEmpty(layoutResponse.adTop) && !TextUtils.isEmpty(layoutResponse.adLeft)){
+
+                String widthStr = layoutResponse.adWidth;
+                String heightStr = layoutResponse.adHeight;
+                String topStr = layoutResponse.adTop;
+                String leftStr = layoutResponse.adLeft;
+
+                //动态布局
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+                double width = (float) (DataUtils.getDisplayMetricsWidth(getActivity())*(Float.parseFloat(widthStr)));
+                double height = (float) (DataUtils.getDisplayMetricsHeight(getActivity())*(Float.parseFloat(heightStr)));
+                double top = (float) (DataUtils.getDisplayMetricsWidth(getActivity())*(Float.parseFloat(topStr)));
+                double left = (float) (DataUtils.getDisplayMetricsHeight(getActivity())*(Float.parseFloat(leftStr)));
+
+                params.width = (int) Math.floor(width);
+                params.height = (int) Math.floor(height);
+                params.topMargin = (int) Math.floor(top);
+                params.leftMargin = (int) Math.floor(left);
+                linearLayout.setLayoutParams(params);
+                LogCat.e(" 广告四布局 width: "+params.width+" height: "+params.height+" top: "+params.topMargin+" left: "+params.leftMargin);
+
+            }
+        }
+
+        return linearLayout;
     }
 
     @Override
@@ -52,8 +98,6 @@ public class ADFourFragment extends BaseFragment {
     @Override
     protected void bindEvent() {
 
-
-
     }
 
     @Override
@@ -67,6 +111,7 @@ public class ADFourFragment extends BaseFragment {
         super.onDestroy();
         if(cycleTextTimer != null){
             cycleTextTimer.cancel();
+            cycleTextTimer = null;
         }
     }
 
@@ -95,6 +140,49 @@ public class ADFourFragment extends BaseFragment {
             super.handleMessage(msg);
         }
     };
+
+
+    /**
+     * 请求文字广告
+     */
+    private void doGetTextAD(){
+        ADHttpService.doHttpGetTextADInfo(getActivity(), new OkHttpCallBack<ADFourResponse>() {
+            @Override
+            public void onSuccess(String url, ADFourResponse response) {
+                LogCat.e(" url " + url);
+                if(!isAdded()){
+                    return;
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+            @Override
+            public void onError(String url, String errorMsg) {
+
+
+
+
+
+
+            }
+        });
+
+    }
+
+
+
 
 
 
