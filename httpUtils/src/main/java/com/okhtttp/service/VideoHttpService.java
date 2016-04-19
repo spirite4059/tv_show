3306cf1d69c8abb7ab0d1fcdb18ab67ee4c9a37a
fuqiang <fuqiang@gochinatv.com>
2016-04-19 11:11:58 +0800

    修改接口

--- httpUtils/src/main/java/com/okhtttp/service/VideoHttpService.java
+++ httpUtils/src/main/java/com/okhtttp/service/VideoHttpService.java
@@ -14,13 +14,22 @@ import java.util.Map;
  */
 public class VideoHttpService {
 
-    private static final String HTTP_URL_GET_VIDEO_LIST = "http://192.168.2.196:8080/api/ad_v1/getAdList";
-
+//    private static final String HTTP_URL_GET_VIDEO_LIST = "http://192.168.2.196:8080/api/ad_v1/getAdList";
+    private static final String HTTP_URL_GET_VIDEO_LIST = "http://210.14.151.100:8090/api/ad_v1/getAdList";
+//                                                           http://210.14.151.100:8090/api/ad_v1/getAdList?mac=cc-79-cf-88-88-88
 
     public static void doHttpGetVideoList(Context context, OkHttpCallBack<AdVideoListResponse> listener){
         Map<String, String> params = new HashMap<>();
 //        params.put("mac", MacUtils.getMacAddress(context));
-        params.put("mac", "rf-ed-rt-ws");
+//        params.put("mac", "rf-ed-rt-ws");
+//        try {
+//            String mac = URLEncoder.encode("cc:79:cf:88:88:88", "UTF-8");
+//            LogCat.e("mac: " + mac);
+//            params.put("mac", mac);
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+        params.put("mac", "cc:79:cf:88:88:88");
         OkHttpUtils.getInstance().doHttpGet(HTTP_URL_GET_VIDEO_LIST, params, listener);
     }
 
