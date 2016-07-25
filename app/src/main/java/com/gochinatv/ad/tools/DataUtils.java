c7f26fb0ee9df4b622015ca2439d2528c6eeb248
zhufengyuan <zhufengyuan@gochinatv.com>
2016-07-25 09:38:57 +0800

    NAbaor的隐藏

--- app/src/main/java/com/gochinatv/ad/tools/DataUtils.java
+++ app/src/main/java/com/gochinatv/ad/tools/DataUtils.java
@@ -544,14 +544,16 @@ public class DataUtils {
 //		activity.getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
 
 
+		if(activity != null){
+			activity.getWindow().getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_IMMERSIVE
+							| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
 
-		activity.getWindow().getDecorView().setSystemUiVisibility(
-				View.SYSTEM_UI_FLAG_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-						| View.SYSTEM_UI_FLAG_IMMERSIVE
-						| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+		}
 
 	}
 
