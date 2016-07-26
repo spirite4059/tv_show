475dc8725c2fee889974ba3c206080e081905be3
zhufengyuan <zhufengyuan@gochinatv.com>
2016-07-26 09:52:35 +0800

    适配遥控器wifi

--- app/src/main/java/com/gochinatv/ad/MainTestActivity.java
+++ app/src/main/java/com/gochinatv/ad/MainTestActivity.java
@@ -1,84 +1,64 @@
-//package com.gochinatv.ad;
-//
-//import android.app.FragmentManager;
-//import android.app.FragmentTransaction;
-//import android.os.Bundle;
-//
-//import com.gochinatv.ad.base.BaseActivity;
-//import com.gochinatv.ad.ui.fragment.ADFourFragment;
-//import com.gochinatv.ad.ui.fragment.ADThreeFragment;
-//import com.httputils.http.response.UpdateResponse;
-//import com.httputils.http.response.VideoDetailListResponse;
-//
-///**
-// * Created by zfy on 2016/3/17.
-// */
-//public class MainTestActivity extends BaseActivity {
-//
-//
-//    //private LinearLayout adOneLayout,adTwoLayout,adThreeLayout,adFourLayout;
-//
-//
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main_test);
-//        initView();
-//        init();
-//    }
-//
-//
-//    private void initView(){
-////        adOneLayout = (LinearLayout) findViewById(R.id.ad_one);
-////        adTwoLayout = (LinearLayout) findViewById(R.id.ad_two);
-////        adThreeLayout = (LinearLayout) findViewById(R.id.ad_three);
-////        adFourLayout = (LinearLayout) findViewById(R.id.ad_four);
-//
-//    }
-//
-//
-//    private void init(){
-//
-//        //添加图片广告
-//        ADThreeFragment fragmentThree = new  ADThreeFragment();
-//        FragmentManager fm3 = getFragmentManager();
-//        FragmentTransaction ft3 = fm3.beginTransaction();
-//        ft3.add(R.id.ad_three,fragmentThree);
-//        ft3.commit();
-//
-//
-//       //添加文本广告
-//        ADFourFragment fragmentFour = new  ADFourFragment();
-//        FragmentManager fm4 = getFragmentManager();
-//        FragmentTransaction ft4 = fm4.beginTransaction();
-//        ft4.add(R.id.ad_four,fragmentFour);
-//        ft4.commit();
-//
-//
-//
-//
-//    }
-//
-//
-//
-//    @Override
-//    protected void onSuccessFul(VideoDetailListResponse response, String turl) {
-//
-//    }
-//
-//    @Override
-//    protected void onFailed(String errorMsg, String turl) {
-//
-//    }
-//
-//    @Override
-//    protected void onUpdateSuccess(UpdateResponse.UpdateInfoResponse updateInfo) {
-//
-//    }
-//
-//
-//
-//
-//}
+package com.gochinatv.ad;
+
+import android.net.wifi.ScanResult;
+import android.os.Bundle;
+
+import com.gochinatv.ad.base.BaseActivity;
+import com.gochinatv.ad.tools.WifiAutoConnectManager;
+import com.gochinatv.ad.ui.dialog.WifiFocusDialog;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by zfy on 2016/3/17.
+ */
+public class MainTestActivity extends BaseActivity {
+
+
+    WifiFocusDialog wifiFocusDialog;
+
+    WifiAutoConnectManager wifiAutoConnectManager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_test);
+        initView();
+        init();
+    }
+
+
+    private void initView(){
+
+
+    }
+
+
+    private void init(){
+
+        if(wifiAutoConnectManager == null){
+            wifiAutoConnectManager = new WifiAutoConnectManager(this);
+        }
+
+        if(!wifiAutoConnectManager.wifiManager.isWifiEnabled()){
+            wifiAutoConnectManager.wifiManager.setWifiEnabled(true);
+        }
+
+
+        wifiAutoConnectManager.wifiManager.startScan();
+
+        wifiFocusDialog = new WifiFocusDialog(MainTestActivity.this, wifiAutoConnectManager, (ArrayList<ScanResult>) wifiAutoConnectManager.wifiManager.getScanResults());
+        wifiFocusDialog.show();
+
+
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/gochinatv/ad/ui/dialog/WifiFocusDialog.java
@@ -0,0 +1,324 @@
+package com.gochinatv.ad.ui.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.Selection;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.gochinatv.ad.MainTestActivity;
+import com.gochinatv.ad.R;
+import com.gochinatv.ad.interfaces.OnWifiConnectListener;
+import com.gochinatv.ad.tools.AlertUtils;
+import com.gochinatv.ad.tools.DataUtils;
+import com.gochinatv.ad.tools.LogCat;
+import com.gochinatv.ad.tools.WifiAutoConnectManager;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ulplanet on 2016/7/25.
+ */
+public class WifiFocusDialog extends Dialog {
+
+    private MainTestActivity mainActivity;
+    private Context context;
+    private CheckBox checkBox;
+    private TextView tvCountTime;//显示30s秒倒计时
+    private EditText etPwd;
+    private Button btnConnect;
+    private Button btnCancel;
+    private Spinner spinner;
+    WifiAutoConnectManager wifiAutoConnectManager;
+    ArrayList<ScanResult> wifiList;
+    WifiAutoConnectManager.WifiCipherType security;
+    ArrayList<WifiInfos> wifoResults;
+    private String wifiName;
+    private Handler handler;
+
+    public WifiFocusDialog(Context context) {
+        super(context);
+    }
+
+
+    public WifiFocusDialog(Context context, WifiAutoConnectManager wifiAutoConnectManager, ArrayList<ScanResult> wifiList) {
+        super(context, R.style.wifiDialog);
+        mainActivity = (MainTestActivity) context;
+        this.context = context;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_focus_wifi);
+        setCanceledOnTouchOutside(false);
+
+        this.wifiAutoConnectManager = wifiAutoConnectManager;
+        this.wifiList = wifiList;
+
+        initView();
+        init();
+        bindEvent();
+
+
+    }
+
+    private void initView() {
+        etPwd = (EditText) findViewById(R.id.et_wifi_pwd);
+        btnConnect = (Button) findViewById(R.id.btn_connect);
+        btnCancel = (Button) findViewById(R.id.btn_cancel);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        tvCountTime = (TextView) findViewById(R.id.tv_count_time);
+        checkBox = (CheckBox) findViewById(R.id.checkbox);
+    }
+
+
+    public void init() {
+
+        //etPwd.requestFocus();
+        btnConnect.requestFocus();
+
+        // 获取扫描的wifi列表
+        wifoResults = new ArrayList<>();
+        if (wifiList != null && wifiList.size() != 0) {
+            for (ScanResult scanResult : wifiList) {
+                WifiInfos wifi = new WifiInfos();
+                wifi.ssid = scanResult.SSID;
+                if (!TextUtils.isEmpty(scanResult.capabilities) && scanResult.capabilities.contains("WPA")) {
+                    wifi.capabilities = WifiAutoConnectManager.WifiCipherType.WIFICIPHER_WPA;
+
+                } else if (!TextUtils.isEmpty(scanResult.capabilities) && scanResult.capabilities.contains("WEP")) {
+                    wifi.capabilities = WifiAutoConnectManager.WifiCipherType.WIFICIPHER_WEP;
+                } else {
+                    wifi.capabilities = WifiAutoConnectManager.WifiCipherType.WIFICIPHER_NOPASS;
+                }
+                wifoResults.add(wifi);
+            }
+        }
+
+        WifiListAdapter wifiListAdapter = new WifiListAdapter(wifoResults);
+        spinner.setAdapter(wifiListAdapter);
+    }
+
+    boolean isFirstSelection = true;
+    private void bindEvent() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                if(isFirstSelection){
+                    isFirstSelection = false;
+                    return;
+                }
+
+                etPwd.setText("");
+
+                WifiInfos wifi = wifoResults.get(pos);
+                if(wifi != null){
+                    wifiName = wifi.ssid;
+                    security = wifi.capabilities;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        });
+
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pwd = etPwd.getText().toString();
+                if(security != WifiAutoConnectManager.WifiCipherType.WIFICIPHER_NOPASS && TextUtils.isEmpty(pwd)){
+                    AlertUtils.alert(getContext(), "请输入wifi密码");
+                    etPwd.requestFocus();
+                    return;
+                }
+                LogCat.e("wifi", "wifiName: " + wifiName);
+                LogCat.e("wifi", "pwd: " + pwd);
+                LogCat.e("wifi", "security: " + security);
+                wifiAutoConnectManager.connect(wifiName, pwd, security, new OnWifiConnectListener() {
+                    @Override
+                    public void onWifiConnect(final boolean isConnect) {
+                        LogCat.e("wifi", "wifi状态: " + isConnect);
+                        btnConnect.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (!isConnect) {
+                                    AlertUtils.alert(getContext(), "热点链接失败,请重试!");
+                                    etPwd.setText("");
+
+                                } else {
+                                    AlertUtils.alert(getContext(), "正在链接热点!");
+                                    /**
+                                     * 隐藏NavigationBar
+                                     */
+                                    DataUtils.hideNavigationBar((Activity) context);
+                                    dismiss();
+                                    if(handler == null){
+                                        handler = new Handler(Looper.getMainLooper());
+                                    }
+
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            if(!DataUtils.isNetworkConnected(getContext())){
+                                                // 5秒后仍然没有网络,就继续显示dialog
+                                                AlertUtils.alert(getContext(), "热点链接失败,请重试!");
+                                                etPwd.setText("");
+                                                show();
+                                            }
+                                        }
+                                    }, 15000);
+
+                                }
+
+                            }
+                        });
+                    }
+                });
+
+
+            }
+        });
+
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                 * 隐藏NavigationBar
+                 */
+                DataUtils.hideNavigationBar((Activity) context);
+                dismiss();
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(etPwd != null){
+                    if(b){
+                        //显示密码
+                        etPwd.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    }else{
+                        //隐藏密码
+                        etPwd.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    }
+
+                    //下面两行代码实现: 输入框光标一直在输入文本后面
+                    Editable etable = etPwd.getText();
+                    Selection.setSelection(etable, etable.length());
+                }
+            }
+        });
+
+        etPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if( mainActivity != null){
+                    if(charSequence.length() > 0){
+                        //停止倒计时
+                        //mainActivity.cancelDialogCountTimer();
+                        tvCountTime.setText("30s");
+                    }else{
+                        //开始倒计时
+                        //mainActivity.startDialogCountTimer();
+                    }
+                }else{
+                    LogCat.e("WifiDialog", "mainActivity == null........");
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+
+    }
+
+
+
+
+    public class WifiInfos {
+        public String ssid;
+        public WifiAutoConnectManager.WifiCipherType capabilities;
+
+    }
+
+
+
+    private class WifiListAdapter extends BaseAdapter {
+
+        private ArrayList<WifiInfos> wifoResults;
+
+        public WifiListAdapter(ArrayList<WifiInfos> wifoResults) {
+            this.wifoResults = wifoResults;
+        }
+
+        @Override
+        public int getCount() {
+            return wifoResults.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return wifoResults.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.item_wifi, null);
+            }
+            WifiInfos wifiInfos = wifoResults.get(i);
+            ((TextView) view).setText(wifiInfos.ssid);
+            return view;
+        }
+
+    }
+
+
+    /**
+     * 显示30s倒计时
+     */
+    public void showCountTime(String time){
+        if(tvCountTime != null){
+            tvCountTime.setText(time+"s");
+        }
+
+    }
+
+
+}
