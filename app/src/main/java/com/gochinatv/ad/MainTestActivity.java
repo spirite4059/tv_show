34a1b912248ab3d4cd33793b5e1b92f6a301aa81
zhufengyuan <zhufengyuan@gochinatv.com>
2016-08-10 09:59:48 +0800

    20160810

--- app/src/main/java/com/gochinatv/ad/MainTestActivity.java
+++ app/src/main/java/com/gochinatv/ad/MainTestActivity.java
@@ -4,6 +4,10 @@ import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.gochinatv.ad.base.BaseActivity;
 import com.gochinatv.ad.tools.WifiAutoConnectManager;
@@ -23,7 +27,7 @@ public class MainTestActivity extends BaseActivity {
 
     WifiAutoConnectManager wifiAutoConnectManager;
 
-
+    LinearLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +39,7 @@ public class MainTestActivity extends BaseActivity {
 
 
     private void initView(){
-
+        root = (LinearLayout) findViewById(R.id.root_image);
 
     }
 
@@ -57,35 +61,54 @@ public class MainTestActivity extends BaseActivity {
 //        wifiFocusDialog.show();
 
 
-
+        getBitmapsFromVideo();
 
     }
 
 
     public void getBitmapsFromVideo() {
         String dataPath = Environment.getExternalStorageDirectory()+ "/testVideo.mp4";
-        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-        retriever.setDataSource(dataPath);
-        // 取得视频的长度(单位为毫秒)
-        String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-        // 取得视频的长度(单位为秒)
-        int seconds = Integer.valueOf(time) / 1000;
-        // 得到每一秒时刻的bitmap比如第一秒,第二秒
-        for (int i = 1; i <= seconds; i=i+4) {
-            Bitmap bitmap = retriever.getFrameAtTime(i*1000*1000,MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
-            String path = Environment.getExternalStorageDirectory()+ File.separator + i + ".jpg";
-            FileOutputStream fos = null;
-            try {
-                fos = new FileOutputStream(path);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fos);
-                fos.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+        if(dataPath != null){
+            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+            retriever.setDataSource(dataPath);
+            // 取得视频的长度(单位为毫秒)
+            String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+            // 取得视频的长度(单位为秒)
+            int seconds = Integer.valueOf(time) / 1000;
+            // 得到每一秒时刻的bitmap比如第一秒,第二秒
+            for (int i = 1; i <= seconds; i=i+4) {
+                Bitmap bitmap = retriever.getFrameAtTime(i*1000*1000,MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
+                addImageView(bitmap);
+                String path = Environment.getExternalStorageDirectory()+ File.separator + i + ".jpg";
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(path);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fos);
+                    fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+        }else{
+            Toast.makeText(this,"视频文件不存在！！！！",Toast.LENGTH_LONG).show();
         }
+
     }
 
 
+    /**
+     * 测试
+     */
+    private void addImageView(Bitmap bitmap){
+        ImageView view = new ImageView(this);
+        view.setImageBitmap(bitmap);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(100,100);
+        view.setLayoutParams(layoutParams);
+
+        if(root != null){
+            root.addView(view);
+        }
+    }
 
 
 
